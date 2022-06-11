4108e56d846dfe44d48a120e57590e43c7923629
Matthew Ballance <matt.ballance@gmail.com>
2022-06-10 17:50:14 -0700

    Update viewer to report a better error on database validation failure
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/pyucis_viewer/__main__.py
+++ src/pyucis_viewer/__main__.py
@@ -4,6 +4,7 @@ Created on Jan 5, 2020
 @author: ballance
 '''
 import argparse
+import traceback
 from pyucis_viewer.main_window import MainWindow
 from pyucis_viewer.data_model import DataModel
 import sys
@@ -23,17 +24,22 @@ def main():
     args = parser.parse_args()
     
     data_model = DataModel()
-
-    app = QApplication(sys.argv)    
-    main_win = MainWindow(data_model)
    
     db = None 
     if args.file.endswith(".xml"):
-        print("xml file")
-        db = XmlFactory.read(args.file)
+        try:
+            db = XmlFactory.read(args.file)
+        except Exception as e:
+            print("Failed to read UCIS file \"%s\": %s" % (
+                args.file,
+                str(e)), flush=True)
+            sys.exit(1)
     else:
         print("Error: unknown suffix for coverage file " + args.file)
         sys.exit(1)
+
+    app = QApplication(sys.argv)    
+    main_win = MainWindow(data_model)
     
     # Notify everyone that a new database is available
     data_model.load(db)
